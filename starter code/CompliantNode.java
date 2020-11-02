4f7a76967e488746c5c645035df74cbfe7a16784
Samantha Ibasitas <36210345+samanthaibasitas@users.noreply.github.com>
2020-11-01 17:17:14 -0800

    Completed CompliantNode.java

--- starter code/CompliantNode.java
+++ starter code/CompliantNode.java
@@ -1,26 +1,40 @@
 import java.util.ArrayList;
 import java.util.Set;
-
+import java.util.HashSet;
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+    public int numRounds;
+    public Set<Transaction> pendingTransactions;
+    public boolean[] followees;
+    public double p_malicious;
+    public double p_txDistribution;
+    public double p_graph;
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.numRounds = numRounds;
+        this.pendingTransactions = new HashSet<Transaction>();
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.p_graph= p_graph;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        this.pendingTransactions = pendingTransactions;
     }
 
-    public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+    public Set<Transaction> getProposals() {
+        return pendingTransactions;
     }
 
     public void receiveCandidates(ArrayList<Integer[]> candidates) {
-        // IMPLEMENT THIS
+        for(Integer[] cand : candidates) { 
+            if (followees[cand[1]]){
+                pendingTransactions.add(new Transaction(cand[0]));
+            }
+        }
     }
 }
